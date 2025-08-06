74357d7f05cc830839742738c6ae5d43478c21da
Flavia Martinez <flavia.martinez@uc.cl>
2025-08-05 23:56:17 -0400

    fetch products from MockAPI

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,6 +1,23 @@
+import { useState, useEffect } from 'react'
+
 const Products = () => {
+  const [products, setProducts] = useState([])
+  const getProducts = async () => {
+    try {
+      const res = await fetch('https://6892b6d4c49d24bce8682399.mockapi.io/api/products')
+      const data = await res.json()
+      return setProducts(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getProducts()
+  }, [])
+
   return (
-    <div>Products</div>
+    <pre>{JSON.stringify(products, null, 2)}</pre>
   )
 }
 
